90608091e4021587a7c5d5d7c4eac49e92cfc600
KAREN <alpaca_karen@yahoo.co.jp>
2015-11-30 23:22:53 +0900

    はいすこあ画面ボタン

---
+++ uty-jin/Highscore.swift
@@ -0,0 +1,20 @@
+//
+//  Highscore.swift
+//  uty-jin
+//
+//  Created by KAREN on 2015/11/30.
+//  Copyright © 2015年 KAREN. All rights reserved.
+//
+
+import WatchKit
+
+class Highscore: NSObject {
+    
+    @IBAction func a(){
+        
+    }
+    @IBAction func b(){
+        
+    }
+
+}
