e2cc01eae7b0d27bd9cc984d423592c8c93be6ac
KAREN <alpaca_karen@yahoo.co.jp>
2015-12-11 19:35:02 +0900

    ハートを加えた

--- uty-jin/GameScene.swift
+++ uty-jin/GameScene.swift
@@ -34,6 +34,10 @@ class GameScene : SKScene, SKPhysicsContactDelegate {
     var hosi = SKSpriteNode()
     var hosia = SKSpriteNode()
     
+    var imageView = UIImageView(frame: CGRectMake(0,200,20,20))
+    var imageViewa = UIImageView(frame: CGRectMake(30,200,40,20))
+    var imageViewb = UIImageView(frame: CGRectMake(60,200,60,20))
+    
     let ballCategory: UInt32 = 0x1 << 0
     let tikyuCategory: UInt32 = 0x1 << 1
     
@@ -97,6 +101,46 @@ class GameScene : SKScene, SKPhysicsContactDelegate {
         hosia.name = "hosia"
         self.addChild(hosia)
         
+        // UIImageViewを作成する.
+        imageView = UIImageView(frame: CGRectMake(0,0,30,30))
+        
+        // 表示する画像を設定する.
+        let image = UIImage(named: "ha-to.png")
+        
+        // 画像をUIImageViewに設定する.
+        imageView.image = image
+        
+        // 画像の表示する座標を指定する.
+        imageView.layer.position = CGPoint(x: 30, y: 70)
+        
+        
+        // UIImageViewをViewに追加する.
+        self.view!.addSubview(imageView)
+        
+        //ハート二つ目
+        
+        imageViewa = UIImageView(frame: CGRectMake(0,30,60,30))
+        
+        let imagea = UIImage(named: "ha-to.png")
+        
+        imageView.image = imagea
+        
+        imageView.layer.position = CGPoint(x: 60, y: 70)
+        
+        self.view!.addSubview(imageView)
+        
+        //ハート３個目
+        imageViewb = UIImageView(frame: CGRectMake(0,30,60,30))
+        
+        let imageb = UIImage(named: "ha-to.png")
+        
+        imageView.image = imageb
+        
+        imageView.layer.position = CGPoint(x: 60, y: 70)
+        
+        self.view!.addSubview(imageView)
+        
+       
         
         
     }
@@ -168,7 +212,6 @@ class GameScene : SKScene, SKPhysicsContactDelegate {
         if let col = contact.bodyA.node {
             if col.name == "tikyu" {
                 // 地球との衝突
-                print("hoge")
                 ball.removeFromParent()
                 fallUtyujin()
             }
@@ -182,13 +225,54 @@ class GameScene : SKScene, SKPhysicsContactDelegate {
                 ball.removeFromParent()
                 fallUtyujin()
         }
+            
     }
+    }
+    class ViewController: UIViewController {
+        
+        private var myImageView: UIImageView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // UIImageViewを作成する.
+            myImageView = UIImageView(frame: CGRectMake(0,0,100,120))
+            
+            // 表示する画像を設定する.
+            let myImage = UIImage(named: ".png")
+            
+            // 画像をUIImageViewに設定する.
+            myImageView.image = myImage
+            
+            // 画像の表示する座標を指定する.
+            myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: 200.0)
+            
+            // UIImageViewをViewに追加する.
+            self.view.addSubview(myImageView)
+        }
+        
+        
+  
+   /* (id)initWithSize:(CGSize)size {
+    self = [super initWithSize:size];
+    if (self) {
+    SKLabelNode *titleLabel = [SKLabelNode labelNodeWithFontNamed:@"HelveticaNeue"];
+    titleLabel.text = @"BREAKOUT!";
+    titleLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    titleLabel.fontSize = 50.0f;
+    [self addChild:titleLabel];
+    }
+    return self;
+    }*/
 }
+
+//効果音をつける
+//スコア画面、ゲーム終了画面を作る
+
+
+
+
 }
-
-
-
-
 
 
 
